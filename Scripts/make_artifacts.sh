e192c206c429cac28366e43a71b727bb439150b5
Peter Jenkins <pjenkins@mparticle.com>
2021-03-03 10:12:52 -0500

    Test 10

--- Scripts/make_artifacts.sh
+++ Scripts/make_artifacts.sh
@@ -7,10 +7,21 @@
 #
 
 # --- Configuration ---
-
+WORK_DIR="$(pwd)"
 ARTIFACT_DIR="$HOME/artifacts"
+TEMP_DIR="$HOME/temp"
 
 # --- Functions ---
+
+function setup() {
+  mkdir -p "$ARTIFACT_DIR"
+  mkdir -p "$TEMP_DIR"
+  cd "$TEMP_DIR"
+  git clone https://github.com/tomaz/appledoc
+  cd appledoc
+  sudo sh install-appledoc.sh
+  cd "$WORK_DIR"
+}
 
 function apply_location_patch() {
   git apply ./Scripts/0001-DISABLE-LOCATION.patch
@@ -19,7 +30,6 @@ function apply_location_patch() {
 }
 
 function build_carthage_artifact() {
-  mkdir -p "$ARTIFACT_DIR"
   rm -rf Carthage
   echo $1 | grep nolocation && apply_location_patch
   echo $1 | grep xcframework || (./Scripts/carthage.sh build --no-skip-current; carthage archive; mv mParticle_Apple_SDK.framework.zip "$ARTIFACT_DIR/$1")
@@ -28,11 +38,6 @@ function build_carthage_artifact() {
 }
 
 function build_docs_artifact() {
-  ROOT="$(pwd)"
-  git clone https://github.com/tomaz/appledoc
-  cd appledoc
-  sudo sh install-appledoc.sh
-  cd "$ROOT"
   appledoc --exit-threshold=2 "./Scripts/AppledocSettings.plist"
   ditto -c -k --sequesterRsrc --keepParent "./Docs/html" "$ARTIFACT_DIR/$1"
 }
@@ -42,10 +47,12 @@ function move_artifacts() {
   # we can't keep them in currect directory because we manipulate git and
   # they will get wiped out, so this moves them back in when it's safe
 
-  ls "$ARTIFACT_DIR" | xargs -n 1 -J % mv % .
+  find "$ARTIFACT_DIR" | xargs -n 1 -J % mv % .
 }
 
 # --- Main impl ---
+
+setup
 
 build_carthage_artifact mParticle_Apple_SDK.framework.zip
 
