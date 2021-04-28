fac7abec75f852402466ba5117a3237e97c7215b
Robert Ing <ring@mparticle.com>
2021-04-28 16:43:23 -0400

    ci: Add semantic release

---
+++ Scripts/release.sh
@@ -0,0 +1,13 @@
+# update version numbers
+VERSION=$1
+
+sed -i '' 's/NSString \*const kMParticleSDKVersion = @".*/NSString *const kMParticleSDKVersion = @"'"$VERSION"'";/' mParticle-Apple-SDK/MPIConstants.m
+/usr/libexec/PlistBuddy -c "Set CFBundleShortVersionString $VERSION" Framework/Info.plist
+jq --indent 3 '. += {'"\"$VERSION\""': "'"https://github.com/mParticle/mparticle-apple-sdk/releases/download/$VERSION/mParticle_Apple_SDK.framework.zip"'"}' mParticle_Apple_SDK.json > tmp.json
+mv tmp.json mParticle_Apple_SDK.json
+sudo npm install -g podspec-bump
+podspec-bump -w -i $VERSION
+# git add mParticle-Apple-SDK/MPIConstants.m Framework/Info.plist; git add mParticle-Apple-SDK.podspec; git add mParticle_Apple_SDK.json; git commit -m "Update version to $VERSION"
+
+./make_artifacts.sh
+ls mParticle_Apple_SDK.framework.zip mParticle_Apple_SDK.framework.nolocation.zip mParticle_Apple_SDK.xcframework.zip mParticle_Apple_SDK.xcframework.nolocation.zip generated-docs.zip || exit 1
---
+++ release.config.js
@@ -0,0 +1,57 @@
+module.exports = {
+  branches: ["master"],
+  tagFormat: "v${version}",
+  repositoryUrl: "https://github.com/mParticle/mparticle-apple-sdk",
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+      },
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "angular",
+      },
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: [
+          "mParticle-Apple-SDK/MPIConstants.m",
+          "Framework/Info.plist",
+          "mParticle-Apple-SDK.podspec",
+          "mParticle_Apple_SDK.json",
+          "CHANGELOG.md",
+        ],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          "mParticle_Apple_SDK.framework.zip",
+          "mParticle_Apple_SDK.framework.nolocation.zip",
+          "mParticle_Apple_SDK.xcframework.zip",
+          "mParticle_Apple_SDK.xcframework.nolocation.zip",
+          "generated-docs.zip",
+        ],
+      },
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: "sh ./Scripts/release.sh ${nextRelease.version}",
+      },
+    ],
+  ],
+};
