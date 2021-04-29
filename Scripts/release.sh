efd07929c8ea029c70b411d30aa45db09bf4a9ce
Peter Jenkins <pjenkins@mparticle.com>
2021-04-29 15:44:52 -0400

    Test 9

--- Scripts/release.sh
+++ Scripts/release.sh
@@ -1,5 +1,6 @@
 # update version numbers
 PREFIXED_VERSION="$1"
+NOTES="$2"
 VERSION="$(echo $1 | sed 's/v//g')"
 
 sed -i '' 's/NSString \*const kMParticleSDKVersion = @".*/NSString *const kMParticleSDKVersion = @"'"$VERSION"'";/' mParticle-Apple-SDK/MPIConstants.m
@@ -8,8 +9,8 @@ jq --indent 3 '. += {'"\"$VERSION\""': "'"https://github.com/mParticle/mparticle
 mv tmp.json mParticle_Apple_SDK.json
 sudo npm install -g podspec-bump
 podspec-bump -w -i $VERSION
-#git add mParticle-Apple-SDK/MPIConstants.m Framework/Info.plist; git add mParticle-Apple-SDK.podspec; git add mParticle_Apple_SDK.json; git add CHANGELOG.md; git commit -m "Prepare release for $VERSION"
-#git push origin master
+git add mParticle-Apple-SDK/MPIConstants.m Framework/Info.plist; git add mParticle-Apple-SDK.podspec; git add mParticle_Apple_SDK.json; git add CHANGELOG.md; git commit -m "chore(release): $VERSION [skip ci]\n\n$NOTES"
+git push origin master
 
 ./Scripts/make_artifacts.sh
 ls mParticle_Apple_SDK.framework.zip mParticle_Apple_SDK.framework.nolocation.zip mParticle_Apple_SDK.xcframework.zip mParticle_Apple_SDK.xcframework.nolocation.zip generated-docs.zip || exit 1
